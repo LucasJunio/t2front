43a08f4f8f4553638cd7b5e15d5e5c34374e064c
LucasJunio <lucas.verdine@gmail.com>
2020-10-30 18:10:30 -0300

    inital implementation completed of the second phase

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -24,20 +24,8 @@ export default function Sidebar() {
                             <span>MAIN LINKS</span>
                             <nav>
                                 <ul>
-                                    <li><Link  to="/admin/"><i className="fa fa-bolt"></i> Departamento </Link></li>
-                                    <li><Link  to="/admin/office"><i className="fa fa-bolt"></i> Cargo </Link></li>
-                                    <li><Link  to="/admin/user"><i className="fa fa-bolt"></i> Usuário </Link></li>
-                                    <li><Link  to="/admin/segment"><i className="fa fa-bolt"></i> Segmento </Link></li>
                                     <li><Link  to="/admin/interest"><i className="fa fa-bolt"></i> Interesse </Link></li>
                                     <li><Link  to="/admin/question"><i className="fa fa-bolt"></i> Pergunta </Link></li>
-                                    <li><Link  to="/admin/contenttype"><i className="fa fa-bolt"></i> Tipo de conteúdo </Link></li>
-                                    <li><Link  to="/admin/company"><i className="fa fa-bolt"></i> Empresa </Link></li>
-                                    <li><Link  to="/admin/content"><i className="fa fa-bolt"></i> Conteúdo </Link></li>
-                                    <li><Link  to="/admin/conquest"><i className="fa fa-bolt"></i> Conquista </Link></li>
-                                    <li><Link  to="/admin/skill"><i className="fa fa-bolt"></i> Competência </Link></li>
-                                    <li><Link  to="/admin/quadrant"><i className="fa fa-bolt"></i> Quadrante </Link></li>
-                                    <li><Link  to="/admin/sublevel"><i className="fa fa-bolt"></i> Subnível </Link></li>
-                                    <li><Link  to="/admin/audience"><i className="fa fa-bolt"></i> Público Alvo </Link></li>
                                     <li><Link  to="/admin/evaluation"><i className="fa fa-bolt"></i> Avaliação </Link></li>
                                 </ul>
                             </nav>
