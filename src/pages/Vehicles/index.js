c524e966513aed306833c901ed2ec9449a43c374
LucasJunio <lucas.verdine@gmail.com>
2021-01-06 10:28:03 -0300

    finish vehicles

--- src/pages/Vehicles/index.js
+++ src/pages/Vehicles/index.js
@@ -55,6 +55,8 @@ export default function Vehicles() {
                     { title: 'Ano', field: 'ano' },
                 ]}
 
+
+
                 data={vehicles}
                 title="Veículos do usuário"
 
